5860ab46ac15eff2da1cb6060b3d60e5121a9964
humamafif <afifalmahbubi04@gmail.com>
2025-06-21 19:51:02 +0700

    :star:new/invoice feature

--- resources/js/components/user-menu-content.tsx
+++ resources/js/components/user-menu-content.tsx
@@ -3,7 +3,7 @@ import { UserInfo } from '@/components/user-info';
 import { useMobileNavigation } from '@/hooks/use-mobile-navigation';
 import { type User } from '@/types';
 import { Link, router } from '@inertiajs/react';
-import { LogOut, Settings } from 'lucide-react';
+import { History, LogOut, Settings } from 'lucide-react';
 
 interface UserMenuContentProps {
     user: User;
@@ -26,6 +26,14 @@ export function UserMenuContent({ user }: UserMenuContentProps) {
             </DropdownMenuLabel>
             <DropdownMenuSeparator />
             <DropdownMenuGroup>
+                {user.role !== 'admin' && (
+                    <DropdownMenuItem asChild>
+                        <Link className="block w-full" href={route('invoice.index')} as="button" prefetch onClick={cleanup}>
+                            <History className="mr-2" />
+                            Riwayat Pembelian
+                        </Link>
+                    </DropdownMenuItem>
+                )}
                 <DropdownMenuItem asChild>
                     <Link className="block w-full" href={route('profile.edit')} as="button" prefetch onClick={cleanup}>
                         <Settings className="mr-2" />
